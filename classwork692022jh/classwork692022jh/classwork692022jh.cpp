eeb4024ba80a4804e34dab6b208cecc4e0237eef
IMYanev18 <IMYanev18@codingburgas.bg>
2022-06-09 09:51:07 +0300

    adding numbers into vector

--- classwork692022jh/classwork692022jh/classwork692022jh.cpp
+++ classwork692022jh/classwork692022jh/classwork692022jh.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    //addition
+    //adding numbers into vector
+
+    vector<int> num{ 1, 2, 3 };
+
+    num.push_back(4);
+    num.push_back(5);
+
+    cout << "vector with added 4 and 5: ";
+
+    for (const int& i : num) {
+        cout << i << "  ";
+    }
 }
 
